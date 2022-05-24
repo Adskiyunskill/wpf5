c7a629adecff3c4a7e7e2f30c90c7d467e199213
Adskiyunskill <64008131+Adskiyunskill@users.noreply.github.com>
2022-05-24 16:20:40 +0300

    F+++

--- WpfApp5/MainWindow.xaml.cs
+++ WpfApp5/MainWindow.xaml.cs
@@ -13,16 +13,107 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp5
+namespace WpfApp5 
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        Brush backgr, foregr, bordbr;
+        Thickness bordth;
         public MainWindow()
         {
             InitializeComponent();
+            grid1.AddHandler(TextBox.TextChangedEvent,
+                new TextChangedEventHandler(textBox1_TextChanged));
+            backgr = textBox1.Background;
+            foregr = textBox1.Foreground;
+            bordbr = textBox1.BorderBrush;
+            bordth = textBox1.BorderThickness;
+            textBox1.Focus();
+        }
+
+        private void textBox1_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = e.Source as TextBox;
+            if (tb == null)
+                return;
+            tb.Foreground = Brushes.Black;
+            tb.Background = Brushes.LightGreen;
+            tb.Select(tb.Text.Length, 0);
+        }
+
+        private void textBox1_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = e.Source as TextBox;
+            if (tb == null)
+                return;
+            tb.Foreground = foregr;
+            tb.Background = backgr;
+        }
+
+        private void radioButton1_Checked(object sender, RoutedEventArgs e)
+        {
+            foreach (var e1 in grid1.Children)
+            {
+                TextBox tb = e1 as TextBox;
+                if (tb != null)
+                    tb.TabIndex = int.MaxValue;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox tb = e.Source as TextBox;
+            if (tb.Text == "")
+            {
+                tb.BorderBrush = Brushes.Red;
+                tb.BorderThickness = new Thickness(1.01);
+                tb.ToolTip = "Это поле пустое. Заполни его.";
+            }
+            else
+            {
+                tb.BorderBrush = bordbr;
+                tb.BorderThickness = bordth;
+                tb.ToolTip = null;
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            bool res = false;
+                foreach (var e1 in grid1.Children)
+            {
+                if ((e1 as Control).BorderBrush == Brushes.Red)
+                {
+                    res = true;
+                    break;
+                }
+                e.Cancel = res;
+            }
+        }
+
+        private void radioButton2_Checked(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < grid1.Children.Count - 1; i++)
+            {
+                TextBox tb = grid1.Children[i] as TextBox;
+                tb.TabIndex = i * (int)Math.Pow(10, i % 3);
+            }
+        }
+
+        private void grid1_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.F2:
+                    radioButton1.IsChecked = true;
+                    break;
+                case Key.F3:
+                    radioButton2.IsChecked = true;
+                    break;
+            }
         }
     }
 }
